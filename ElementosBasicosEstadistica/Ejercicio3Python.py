13cc44ef75b70f38090a35efbeec00414af54d7f
Alejandro <cayetano.verduzco.alejandro@gmail.com>
2025-04-02 09:09:12 -0600

    Ejercicio 3 Manhattan y Chebyshev

--- ElementosBasicosEstadistica/Ejercicio3Python.py
+++ ElementosBasicosEstadistica/Ejercicio3Python.py
@@ -22,7 +22,7 @@ df_puntos.columns = ['X','Y']
 print("Coordenadas de las tiendas: ")
 print(df_puntos)
 
-def calcular_distancias(puntos):
+def calcular_distancias_euclidian(puntos):
     distancias = pd.DataFrame(index = df_puntos.index,columns = df_puntos.index)
     # Cálculo de distancias
     for i in df_puntos.index:
@@ -31,22 +31,49 @@ def calcular_distancias(puntos):
                 # Distancia Euclidiana
                 distancias.loc[i,j] = distance.euclidean(df_puntos.loc[i], df_puntos.loc[j])
     return distancias
-distancias = calcular_distancias(puntos)
-valor_maximo = distancias.values.max()
-(punto1,punto2) = distancias.stack().idxmax()
-print("Tabla de Distancias")
-print(distancias)
-print("Distancia Máxima: ", valor_maximo)
-print("Entre el punto ", punto1, "; y el punto ", punto2)
+
+def calcular_distancias_manhattan(puntos):
+    distancias = pd.DataFrame(index=puntos.index, columns=puntos.index)
+    for i in puntos.index:
+        for j in puntos.index:
+            if i != j: # No calcila la distancia del mismo punto
+                # Distancia Euclidiana
+                distancias.loc[i, j] = distance.cityblock(puntos.loc[i], puntos.loc[j])
+    return distancias
+
+def calcular_distancias_chebyshev(puntos):
+    distancias = pd.DataFrame(index=puntos.index, columns=puntos.index)
+    for i in puntos.index:
+        for j in puntos.index:
+            if i != j: # No calcila la distancia del mismo punto
+                # Distancia Euclidiana
+                distancias.loc[i, j] = distance.chebyshev(puntos.loc[i], puntos.loc[j])
+    return distancias
+
+distancias_euclidianas = calcular_distancias_euclidian(df_puntos)
+distancias_manhattan = calcular_distancias_manhattan(df_puntos)
+distancias_chebyshev = calcular_distancias_chebyshev(df_puntos)
+
+valor_maximo = distancias_euclidianas.values.max()
+(punto1,punto2) = distancias_euclidianas.stack().idxmax()
+
+print(' \nTabla de Distancias Euclidianas:')
+print(distancias_euclidianas)
+print(' \nTabla de Distancias Manhattan:')
+print(distancias_manhattan)
+print(' \nTabla de Distancias Chebyshev:')
+print(distancias_chebyshev)
+print('Distancia Máxima: ', valor_maximo)
+print('Entre el punto', punto1, 'y el punto', punto2)
 
 # Otra manera
-max_value = distancias.max().max()
+max_value = distancias_euclidianas.max().max()
 
 # Obtener la columna que contiene el valor máximo
-col_max = distancias.max().idxmax()
+col_max = distancias_euclidianas.max().idxmax()
 
 # Obtener el índice (fila) que contiene el valor máximo
-id_max = distancias[col_max]
+id_max = distancias_euclidianas[col_max]
 
 print(f"Valor máximo: {max_value}")
 print(f"Columna: {col_max}")
